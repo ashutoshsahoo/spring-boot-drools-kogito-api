98accd0ebd4be7eb425f867d20c1fc307ce3a6f1
Ashutosh Sahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2025-11-21 00:47:08 +0530

    first commit

---
+++ src/main/java/in/ashu/practice/KogitoApplication.java
@@ -0,0 +1,17 @@
+package in.ashu.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+        "in.ashu.practice.**",
+        "org.kie.kogito.**",
+        "http**"
+})
+public class KogitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KogitoApplication.class, args);
+    }
+
+}
